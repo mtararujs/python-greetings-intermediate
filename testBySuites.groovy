d240478ed77c2b283f210f85ae834bf008f1a0d9
Mārtiņš Tararujs <martins.tararujs@testdevlab.com>
2024-01-06 01:20:44 +0200

    Cleanup

--- testBySuites.groovy
+++
@@ -1,5 +0,0 @@
-def testBySuites(String test_environment, String suite){
-    echo "Testing of python-greetings-app on ${test_environment} environment is starting targeting ${suite} suite"
-}
-
-return this;
